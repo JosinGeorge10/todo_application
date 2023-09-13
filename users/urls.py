5380b17f56c5631350a5322729a5a42a1ce7ac64
JosinGeorge10 <josingkollam@gmail.com>
2023-09-13 17:41:18 +0530

    path connected

--- users/urls.py
+++ users/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('create_task/', views.create_task, name='create_task'),
-    path('edit_task/<int:id>/', views.edit_task, name='edit_task'),
-    path('delete_task/<int:id>/', views.delete_task, name='delete_task'),
-    path('finish_task/<int:id>/', views.finish_task, name='finish_task'),
-    path('revise_task/<int:id>/', views.revise_task, name='revise_task'),
+ 
 ]
